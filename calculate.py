d859787d5b1274ae133ba6734f517bf9cc303ecb
sourav164 <sourav.abe@gmail.com>
2023-03-24 16:08:26 -0500

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,7 +1,8 @@
-import os
+import os, shutil
 import numpy as np
+# from plxscripting.easy import *
 
-
+file_path = "C:/Users/sourav/Downloads/Cell127_real properties_python.p3d"
 sres = np.linspace(0.01, 0.1, num=2)
 gn = np.linspace(1, 10, num=2)
 ga = np.linspace(1, 4, num=2)
@@ -16,9 +17,9 @@ for sres_val in sres:
 		for ga_val in gn:
 			for gl_val in gl:
 				for kx_val in kx:
-					print () 
+					print (f'{i} out of {simulation} simulation is being conducted') 
 
-					g_i._gotosoil()
+					g_i.gotosoil()
 
 					g_i.Asphalt.SaturationResidual.set(sres_val)
 					g_i.Asphalt.GenuchtenGn.set(gn_val)
@@ -28,6 +29,7 @@ for sres_val in sres:
 					g_i.Asphalt.PermHorizontalSecondary.set(kx_val)
 					g_i.Asphalt.PermVertical.set(kx_val)
 
+					g_i.gotostages()
 
 					g_i.InitialPhase.ShouldCalculate.set(True)
 					g_i.Phase_1.ShouldCalculate.set(True)
@@ -37,8 +39,8 @@ for sres_val in sres:
 					g_i.calculate()
 					g_i.save()
 
-					file_name = "out\\"+str(sres_val)+str(gn_val)+str(ga_val)+str(gl_val)+str(kx_val)+".p3d"
+					file_name = "out/"+str(sres_val)+str(gn_val)+str(ga_val)+str(gl_val)+str(kx_val)+".p3d"
 					copy_file = os.path.join(os.path.split(file_path)[0], file_name)
 					shutil.copy2(file_path, copy_file)
 
-
+					print (f'File saved in : {copy_file}') 
