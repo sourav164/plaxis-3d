e24417b7da237e4a9e9459397117bf0b2ba16257
sourav164 <sourav.abe@gmail.com>
2023-03-24 15:53:21 -0500

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -8,21 +8,25 @@ ga = np.linspace(1, 4, num=2)
 gl = np.linspace(-3, 3, num=2)
 kx = np.linspace(.001, 1, num=2)
 
+simulation = len(sres)*len(gn)*len(ga)*len(gl)*len(kx)
+
+i = 0
 for sres_val in sres:
 	for gn_val in gn:
 		for ga_val in gn:
 			for gl_val in gl:
 				for kx_val in kx:
-					#2695> _gotosoil
+					print () 
+
 					g_i._gotosoil()
 
-					g_i.Asphalt.SaturationResidual.set(0.02)
-					g_i.Asphalt.GenuchtenGn.set(6)
-					g_i.Asphalt.GenuchtenGa.set(4)
-					g_i.Asphalt.GenuchtenGl.set(2)
-					g_i.Asphalt.PermHorizontalPrimary.set(0.05)
-					g_i.Asphalt.PermHorizontalSecondary.set(.05)
-					g_i.Asphalt.PermVertical.set(.05)
+					g_i.Asphalt.SaturationResidual.set(sres_val)
+					g_i.Asphalt.GenuchtenGn.set(gn_val)
+					g_i.Asphalt.GenuchtenGa.set(ga_val)
+					g_i.Asphalt.GenuchtenGl.set(gl_val)
+					g_i.Asphalt.PermHorizontalPrimary.set(kx_val)
+					g_i.Asphalt.PermHorizontalSecondary.set(kx_val)
+					g_i.Asphalt.PermVertical.set(kx_val)
 
 
 					g_i.InitialPhase.ShouldCalculate.set(True)
@@ -37,4 +41,4 @@ for sres_val in sres:
 					copy_file = os.path.join(os.path.split(file_path)[0], file_name)
 					shutil.copy2(file_path, copy_file)
 
-					
+
