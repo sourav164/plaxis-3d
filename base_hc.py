40c07f7d4951071a5a6aa15ec0489823e272ba4d
sourav164 <sourav.abe@gmail.com>
2023-06-30 12:15:56 -0500

    hc code changed

--- base_hc.py
+++ base_hc.py
@@ -1,31 +1,47 @@
-_set DischargeFunction_9.Table[0].DeltaDischarge 0.3
-_set DischargeFunction_9.Table[1].DeltaDischarge 0.3
 
-_set DischargeFunction_9.Table[1].Time 0.05
+hc_val = 
+time = 
+base_val = 
+sub_grade = 
+sub_val = 
 
-_set Precipitation.DischargeFunction Phase_4 DischargeFunction_9
 
-
-
-_gotosoil
-_set Base.PermHorizontalPrimary 0.5
-
-_set Base.PermHorizontalSecondary 0.5
-_set Base.PermVertical 0.5
+g_i.DischargeFunction_9.Table[0].DeltaDischarge.set(hc_val)
+g_i.DischargeFunction_9.Table[1].DeltaDischarge.set(hc_val)
+g_i.DischargeFunction_9.Table[1].Time(time)
+g_i.Precipitation.DischargeFunction Phase_4 DischargeFunction_9
 
 
 
+g_i.gotosoil()
 
-2972> _set Subgrade.PermHorizontalPrimary 0.6
-Subgrade.PermHorizontalPrimary: 0.6 m/day
-2973> _set Subgrade.PermHorizontalSecondary 0.6
-Subgrade.PermHorizontalSecondary: 0.6 m/day
-2974> _set Subgrade.PermVertical 0.6
-Subgrade.PermVertical: 0.6 m/day
-2975> _set Subbase.PermHorizontalPrimary 1
-Subbase.PermHorizontalPrimary: 1 m/day
-2976> _set Subbase.PermHorizontalSecondary 1
-Subbase.PermHorizontalSecondary: 1 m/day
-2977> _set Subbase.PermVertical 1
-Subbase.PermVertical: 1 m/day
+# Base change
 
+g_i.Base.PermHorizontalPrimary.set(base_val)
+g_i.Base.PermHorizontalSecondary.set(base_val)
+g_i.Base.PermVertical.set(base_val)
+
+
+# subgrade change
+g_i.Subgrade.PermHorizontalPrimary.set(sub_grade)
+g_i.Subgrade.PermHorizontalSecondary.set(sub_grade)
+g_i.Subgrade.PermVertical.set(sub_grade)
+
+
+# subbase change
+g_i.Subbase.PermHorizontalPrimary.set(sub_val)
+g_i.Subbase.PermHorizontalSecondary.set(sub_val)
+g_i.Subbase.PermVertical.set(sub_val)
+
+
+# build and calculate
+
+g_i.gotostages()
+g_i.InitialPhase.ShouldCalculate.set(True)
+g_i.Phase_1.ShouldCalculate.set(True)
+g_i.Phase_2.ShouldCalculate.set(True)
+g_i.Phase_3.ShouldCalculate.set(True)
+g_i.Phase_4.ShouldCalculate.set(True)
+
+g_i.calculate()
+g_i.save()
